e6a3aede3e7093d647d7a94f4faade97d28535d2
Duygu Tümer <69381839+duygutumer@users.noreply.github.com>
2025-01-04 22:56:06 +0300

    keys are added to env

--- app/__init__.py
+++ app/__init__.py
@@ -5,32 +5,32 @@ import logging
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_limiter.errors import RateLimitExceeded
+from dotenv import load_dotenv
 
 limiter = Limiter(
     key_func=get_remote_address,  # IP adresine göre sınırlandırma
     default_limits=["100 per hour"]  # Varsayılan limit
 )
 
+load_dotenv()
+
 def create_app():
     app = Flask(__name__)
 
     # Security Configurations
-    app.config['DEBUG'] = os.getenv('FLASK_DEBUG', 'False').lower() == 'true'  # Use environment variable for debug mode
-    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', os.urandom(24))  # Use environment variable or secure random key
-    app.config['UPLOAD_FOLDER'] = '/secure/uploads'  # Secure folder for uploads
+    app.config['DEBUG'] = os.getenv('FLASK_DEBUG', 'False').lower() == 'true'  # Debug mode
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')  
     app.config['SESSION_COOKIE_HTTPONLY'] = True  # Prevent JavaScript access to session cookie
-    app.config['SESSION_COOKIE_SECURE'] = True  # Send cookies only over HTTPS
-    app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)  # Session expires after 30 minutes
-    app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024  # Limit file uploads to 10 MB
-    app.config['RECAPTCHA_SITE_KEY'] = "6Lc0fKgqAAAAAHTuXoEj4kOO-4RObKWZyIiTRa8O" 
-    app.config['RECAPTCHA_SECRET_KEY'] = "6Lc0fKgqAAAAADF9HW-lU0Vi8JBhF4jL_3693v_n"
-    app.config['VERIFY_URL'] = "https://www.google.com/recaptcha/api/siteverify"
+    app.config['SESSION_COOKIE_SECURE'] = True  #Send cookies only over HTTPS
+    app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)  # Session lifetime
+    app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024  #limit file uploads to 10 MB
+    app.config['RECAPTCHA_SITE_KEY'] = os.getenv('RECAPTCHA_SITE_KEY')  # Recaptcha site key
+    app.config['RECAPTCHA_SECRET_KEY'] = os.getenv('RECAPTCHA_SECRET_KEY')  # Recaptcha secret key
+    app.config['VERIFY_URL'] = os.getenv('VERIFY_URL')  # Recaptcha verification URL
+
 
     limiter.init_app(app)  # Limiter ile Flask uygulamasını bağla
 
-    # Create upload folder if it doesn't exist
-    if not os.path.exists(app.config['UPLOAD_FOLDER']):
-        os.makedirs(app.config['UPLOAD_FOLDER'])
 
     # Initialize database setup
     from app.database import init_db
@@ -51,12 +51,12 @@ def create_app():
 
     @app.errorhandler(500)
     def global_internal_server_error(e):
-        logging.error(f"Internal server error: {str(e)}")  # Log 500 errors for auditing
+        logging.error("Internal server error occurred. Ensure detailed logs are reviewed securely.")
         return "An unexpected error occurred. Please try again later.", 500
     
     @app.errorhandler(RateLimitExceeded)
     def rate_limit_exceeded(e):
-        logging.error(f"Too many attemps: {str(e)} {get_remote_address}")  
+        logging.error(f"Too many attempts from user.")
         # Log 500 errors for auditing
         return render_template_string("Siteye aşırı seviyede istekte bulundunuz. Sonra tekrar deneyiniz."), 429
 
